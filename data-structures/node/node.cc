221da80b04326a6c880e9f5e5cd796851beb1c7d
Wilmer S Zuna Largo <zunalargowilmer@gmail.com>
2021-07-28 23:47:12 -0700

    Added property fields to node for more general usability

--- data-structures/node/node.cc
+++ data-structures/node/node.cc
@@ -7,23 +7,25 @@
 template<class NodeData>
 Node<NodeData>::Node() {
   next = nullptr;
+  back = nullptr;
   nodeHash = -1;
 }
 
 template<class NodeData>
 Node<NodeData>::Node(NodeData data, int dataHash) {
   next = nullptr;
+  back = nullptr;
   value = data;
   nodeHash = dataHash;
 }
 
 std::ostream& operator<<(std::ostream& os, Node<std::string> DataNode) {
-  os << "{Value: " << DataNode.value << ", Hash: " << DataNode.nodeHash << ", Next: " << DataNode.next << "}";
+  os << "{Value: " << DataNode.value << ", Hash: " << DataNode.nodeHash << ", Back: " << DataNode.back << ", Next: " << DataNode.next << "}";
   return os; 
 }
 
 std::ostream& operator<<(std::ostream& os, Node<int> DataNode) {
-  os << "{Value: " << DataNode.value << ", Hash: " << DataNode.nodeHash << ", Next: " << DataNode.next << "}";
+  os << "{Value: " << DataNode.value << ", Hash: " << DataNode.nodeHash << ", Back: " << DataNode.back << ", Next: " << DataNode.next << "}";
   return os; 
 }
 
